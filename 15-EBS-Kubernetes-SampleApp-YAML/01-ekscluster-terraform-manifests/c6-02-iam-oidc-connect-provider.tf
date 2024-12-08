00f740b372f78ceb649d16966b099c94eb93cf0d
XUeshuo Li <spoonlee24k@gmail.com>
2024-12-08 22:23:29 +1100

    completed S15

--- 15-EBS-Kubernetes-SampleApp-YAML/01-ekscluster-terraform-manifests/c6-02-iam-oidc-connect-provider.tf
+++ 15-EBS-Kubernetes-SampleApp-YAML/01-ekscluster-terraform-manifests/c6-02-iam-oidc-connect-provider.tf
@@ -2,12 +2,13 @@
 # Use this data source to lookup information about the current AWS partition in which Terraform is working
 data "aws_partition" "current" {}
 
-# Resource: AWS IAM Open ID Connect Provider
+# Resource: AWS IAM Open ID Connect Provider https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_openid_connect_provider
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
-  thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
-  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  thumbprint_list = [var.eks_oidc_root_ca_thumbprint] # ! referencing to c6-01
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer # ! the EKS cluster itself can act as an OIDC provider
 
+  # https://developer.hashicorp.com/terraform/language/functions/merge
   tags = merge(
     {
       Name = "${var.cluster_name}-eks-irsa"
@@ -16,13 +17,15 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
   )
 }
 
+# --------------------------------------------------------------------------------------------------------------------------------
+# outputs from the open id connect provider so that we can access the open id connect provider info in project 2 
 # Output: AWS IAM Open ID Connect Provider ARN
 output "aws_iam_openid_connect_provider_arn" {
   description = "AWS IAM Open ID Connect Provider ARN"
   value = aws_iam_openid_connect_provider.oidc_provider.arn 
 }
 
-# Extract OIDC Provider from OIDC Provider ARN
+# Extract OIDC Provider from OIDC Provider ARN (basically a string extraction)
 locals {
     aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
 }
--- 15-EBS-Kubernetes-SampleApp-YAML/02-ebs-terraform-manifests/c4-04-ebs-csi-install-using-helm.tf
+++ 15-EBS-Kubernetes-SampleApp-YAML/02-ebs-terraform-manifests/c4-04-ebs-csi-install-using-helm.tf
@@ -1,15 +1,16 @@
 # Install EBS CSI Driver using HELM
-# Resource: Helm Release 
+# Resource: Helm Release  https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
+# ! helm release resource is just like executing helm CLI, you can ask gpt to convert below code to helm CLI
 resource "helm_release" "ebs_csi_driver" {
-  depends_on = [aws_iam_role.ebs_csi_iam_role]
-  name       = "${local.name}-aws-ebs-csi-driver"
-  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  depends_on = [aws_iam_role.ebs_csi_iam_role]   # this explicit dependency ensures that the IAM Role is created before the Helm Release is attempted
+  name       = "${local.name}-aws-ebs-csi-driver" # ! needs to match IAM Role Condition in c4-02
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver" 
   chart      = "aws-ebs-csi-driver"
-  namespace = "kube-system"     
+  namespace = "kube-system"      # ! needs to match IAM Role Condition in c4-02
 
   set {
     name = "image.repository"
-    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+    value = "602401143452.dkr.ecr.ap-southeast-2.amazonaws.com/eks/aws-ebs-csi-driver" # ! Changes based on Region - This is for ap-southeast-2 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
   }       
 
   set {
