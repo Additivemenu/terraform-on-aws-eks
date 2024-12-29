80923875fe26d61175bae4f06e60328a03a4b937
XUeshuo Li <spoonlee24k@gmail.com>
2024-12-29 19:58:22 +1100

    completed section17

--- 17-EBS-Resizing-on-EKS/01-ekscluster-terraform-manifests/c5-03-iamrole-for-eks-cluster.tf
+++ 17-EBS-Resizing-on-EKS/01-ekscluster-terraform-manifests/c5-03-iamrole-for-eks-cluster.tf
@@ -1,4 +1,5 @@
 # Create IAM Role
+# ! an IAM role can be attached with multiple policies
 resource "aws_iam_role" "eks_master_role" {
   name = "${local.name}-eks-master-role"
 
@@ -18,14 +19,17 @@ resource "aws_iam_role" "eks_master_role" {
 POLICY
 }
 
-# Associate IAM Policy to IAM Role
+# ! Associate IAM Policy to IAM Role
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy" # you can search this policy in AWS IAM Console 
+  # This policy provides Kubernetes the permissions it requires to manage resources on your behalf. 
+  # Kubernetes requires Ec2:CreateTags permissions to place identifying information on EC2 resources including but not limited to Instances, Security Groups, and Elastic Network Interfaces.
   role       = aws_iam_role.eks_master_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController" # you can search this policy in AWS IAM Console
+  # Policy used by VPC Resource Controller to manage ENI and IPs for worker nodes
   role       = aws_iam_role.eks_master_role.name
 }
 
--- 17-EBS-Resizing-on-EKS/02-ebs-terraform-manifests/c2-remote-state-datasource.tf
+++ 17-EBS-Resizing-on-EKS/02-ebs-terraform-manifests/c2-remote-state-datasource.tf
@@ -1,8 +1,9 @@
 # Terraform Remote State Datasource - Remote Backend AWS S3
+# !get cluster info in project 01-eks-terraform-manifests
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket = "terraform-on-aws-eks"
+    bucket = "terraform-on-aws-eks-shawn6237"
     key    = "dev/eks-cluster/terraform.tfstate"
     region = var.aws_region
   }
