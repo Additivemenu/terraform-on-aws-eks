3cb8404a4e58860093344e9b6db326e33ac3b23f
XUeshuo Li <spoonlee24k@gmail.com>
2025-01-01 10:03:11 +1100

    init S22

--- 22-EKS-Admins-with-AWS-IAM-Roles/01-ekscluster-terraform-manifests/c5-06-eks-cluster.tf
+++ 22-EKS-Admins-with-AWS-IAM-Roles/01-ekscluster-terraform-manifests/c5-06-eks-cluster.tf
@@ -1,25 +1,34 @@
 # Create AWS EKS Cluster
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "${local.name}-${var.cluster_name}"
-  role_arn = aws_iam_role.eks_master_role.arn
+  role_arn = aws_iam_role.eks_master_role.arn   # reference to the IAM Role for EKS Master Node
   version = var.cluster_version
 
   vpc_config {
-    subnet_ids = module.vpc.public_subnets
+    subnet_ids = module.vpc.public_subnets   # ! Amazon EKS creates cross-account elastic network interfaces in these subnets to allow communication between your worker nodes and the Kubernetes control plane.
+  /*
+  Amazon EKS creates cross-account elastic network interfaces in these subnets to allow communication between your worker nodes and the Kubernetes control plane.
+  */
     endpoint_private_access = var.cluster_endpoint_private_access
     endpoint_public_access  = var.cluster_endpoint_public_access
     public_access_cidrs     = var.cluster_endpoint_public_access_cidrs    
   }
 
   kubernetes_network_config {
-    service_ipv4_cidr = var.cluster_service_ipv4_cidr
+    service_ipv4_cidr = var.cluster_service_ipv4_cidr 
+    /*
+      (Optional) The CIDR block to assign Kubernetes pod and service IP addresses from. 
+      If you don't specify a block, Kubernetes assigns addresses from either the 10.100.0.0/16 or 172.20.0.0/16 CIDR blocks. 
+      We recommend that you specify a block that does not overlap with resources in other networks that are peered or connected to your VPC.
+    */
   }
   
-  # Enable EKS Cluster Control Plane Logging
+  # ! Enable EKS Cluster Control Plane Logging
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  # ! Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # ! Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
--- 22-EKS-Admins-with-AWS-IAM-Roles/01-ekscluster-terraform-manifests/c5-07-eks-node-group-public.tf
+++ 22-EKS-Admins-with-AWS-IAM-Roles/01-ekscluster-terraform-manifests/c5-07-eks-node-group-public.tf
@@ -1,16 +1,17 @@
 # Create AWS EKS Node Group - Public
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group
 resource "aws_eks_node_group" "eks_ng_public" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
 
   node_group_name = "${local.name}-eks-ng-public"
-  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
-  subnet_ids      = module.vpc.public_subnets
-  version = var.cluster_version #(Optional: Defaults to EKS Cluster Kubernetes version)    
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn   # ! important
+  subnet_ids      = module.vpc.public_subnets # !which makes the node group public
+  #version = var.cluster_version #(Optional: Defaults to EKS Cluster Kubernetes version)    
   
   ami_type = "AL2_x86_64"  
   capacity_type = "ON_DEMAND"
   disk_size = 20
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.medium"] # remember k8s worker nodes needs a minimum of 2 vCPUs and 4GB of memory
   
   
   remote_access {
@@ -25,12 +26,12 @@ resource "aws_eks_node_group" "eks_ng_public" {
 
   # Desired max percentage of unavailable worker nodes during node group update.
   update_config {
-    max_unavailable = 1    
-    #max_unavailable_percentage = 50    # ANY ONE TO USE
+    max_unavailable = 1    # (Optional) Desired max number of unavailable worker nodes during node group update.
+    #max_unavailable_percentage = 50    # ANY ONE TO USE (Optional) Desired max percentage of unavailable worker nodes during node group update.
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  # !Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # !Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
